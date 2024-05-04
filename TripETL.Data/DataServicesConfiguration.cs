f39623f15ae5dbfc6260fde8da0af77c9cacdfae
ahopchanga <sangcheoneun@icloud.com>
2024-05-04 14:21:44 +0300

    Add repository DI

--- TripETL.Data/DataServicesConfiguration.cs
+++ TripETL.Data/DataServicesConfiguration.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TripETL.Domain.Interfaces;
 
 namespace TripETL.Data;
 
 public static class DataServicesConfiguration
 {
-    public static void ConfigureDataServices(this IServiceCollection services, IConfiguration config)
+    public static void ConfigureDataServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<TripDbContext>(options =>
-            options.UseSqlServer(
-                config.GetConnectionString("DefaultConnection"),
-                x => x.MigrationsAssembly("TripETL.Data")
-            )
-        );
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<ITripRepository, TripRepository>();
     }
 }
---
+++ TripETL.Data/TripRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TripETL.Domain.Entities;
+using TripETL.Domain.Interfaces;
+
+namespace TripETL.Data;
+
+public class TripRepository : ITripRepository
+{
+    
+    private readonly TripDbContext _dbContext;
+
+    public async Task<Trip> GetByIdAsync(int id)
+    {
+        return await _dbContext.Trips.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Trip>> GetAllAsync()
+    {
+        return await _dbContext.Trips.ToListAsync();
+    }
+
+    public async Task AddAsync(Trip entity)
+    {
+        await _dbContext.Trips.AddAsync(entity);
+        await _dbContext.SaveChangesAsync();    
+    }
+
+    public async Task UpdateAsync(Trip entity)
+    {
+        _dbContext.Trips.Update(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var trip = await GetByIdAsync(id);
+        if (trip != null)
+        {
+            _dbContext.Trips.Remove(trip);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+    public async Task<Trip> GetByDetailsAsync(DateTime pickup, DateTime dropoff, int passengerCount)
+    {
+        return await _dbContext.Trips.FirstOrDefaultAsync(
+            trip => trip.TpepPickupDatetime == pickup && 
+                    trip.TpepDropoffDatetime == dropoff && 
+                    trip.PassengerCount == passengerCount);
+    }
+
+    public async Task<IEnumerable<Trip>> GetLongestDistancesAsync(int count)
+    {
+        return await _dbContext.Trips
+            .OrderByDescending(trip => trip.TripDistance)
+            .Take(count)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Trip>> GetLongestTimesAsync(int count)
+    {
+        return await _dbContext.Trips
+            .OrderByDescending(trip => trip.TpepDropoffDatetime - trip.TpepPickupDatetime)
+            .Take(count)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Trip>> SearchByPickupIdAsync(int pickupId)
+    {
+        return await _dbContext.Trips
+            .Where(trip => trip.PULocationId == pickupId)
+            .ToListAsync();
+    }
+
+    public async Task BulkInsertAsync(IEnumerable<Trip> trips)
+    {
+        await _dbContext.Trips.AddRangeAsync(trips);
+        await _dbContext.SaveChangesAsync();
+    }
+}
